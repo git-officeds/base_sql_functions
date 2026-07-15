338c87908b7df7c524058abb91b59d41b263eabb
officeds <info@officeds.jp>
2026-07-15 13:41:49 +0900

    初回コミット

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    require_once("sql.php");
+    $fields = ['title', 'content'];
+    $dbget = sql_search_user('post', ['title', 'content'], 'カフェ 市販のルー', 'post_at DESC');
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>.container{ width:100%; max-width:700px; margin:auto;  }</style>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <!--<script src="script.js" defer></script>-->
+</head>
+
+<body>
+    <div class="container">
+        <?php foreach($dbget as $get_data): ?>
+            <div class="get">
+                <p><?= $get_data['post_at'] ?></p>
+                <p><?= $get_data['title'] ?></p>
+                <p><?= $get_data['content'] ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
